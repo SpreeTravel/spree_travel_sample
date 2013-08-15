4d400e279bcb973517acb5341063926cca65e153
snc <snc@jaf.com>
2013-08-15 07:52:43 -0500

    removed unnecessary directory

--- lib/spree/travel_sample.rb
+++ lib/spree/travel_sample.rb
@@ -1,17 +1,17 @@
 module Spree
-  module Sample
-    def self.load_sample(file)
-      path = File.expand_path(samples_path + "#{file}.rb")
+  module TravelSample
+    def self.load_travel_sample(file)
+      path = File.expand_path(travel_samples_path + "#{file}.rb")
       # Check to see if the specified file has been loaded before
       if !$LOADED_FEATURES.include?(path)
         require path
-        puts "Loaded #{file.titleize} samples"
+        puts "Loaded #{file.titleize} travel samples"
       end
     end
 
     private
-      def self.samples_path
-        Pathname.new(File.join(File.dirname(__FILE__), '..', '..', 'db', 'samples'))
+      def self.travel_samples_path
+        Pathname.new(File.join(File.dirname(__FILE__), '..', '..', 'db', 'travel_samples'))
       end
   end
 end
