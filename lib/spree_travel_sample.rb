5d5627e6351e72ae584226d7ab85b44e14e6bb6c
Miguel Sancho <sanchojaf@gmail.com>
2013-11-05 22:35:10 -0500

    addesd namespace SpreeTravel

--- lib/spree_travel_sample.rb
+++ lib/spree_travel_sample.rb
@@ -8,35 +8,35 @@ module SpreeTravelSample
     # Needs to be here so we can access it inside the tests
     def self.load_travel_samples
         
-      Spree::TravelSample.load_travel_sample("option_types")
-      Spree::TravelSample.load_travel_sample("option_values")
+      SpreeTravel::TravelSample.load_travel_sample("option_types")
+      SpreeTravel::TravelSample.load_travel_sample("option_values")
 
-      Spree::TravelSample.load_travel_sample("property_types")      
-      Spree::TravelSample.load_travel_sample("properties_flight")
-      Spree::TravelSample.load_travel_sample("properties_hotel")
-      Spree::TravelSample.load_travel_sample("properties_program")
-      Spree::TravelSample.load_travel_sample("properties_rent_a_car")
-      Spree::TravelSample.load_travel_sample("properties_tour")
-      Spree::TravelSample.load_travel_sample("properties_transfer")
+      SpreeTravel::TravelSample.load_travel_sample("property_types")      
+      SpreeTravel::TravelSample.load_travel_sample("properties_flight")
+      SpreeTravel::TravelSample.load_travel_sample("properties_hotel")
+      SpreeTravel::TravelSample.load_travel_sample("properties_program")
+      SpreeTravel::TravelSample.load_travel_sample("properties_rent_a_car")
+      SpreeTravel::TravelSample.load_travel_sample("properties_tour")
+      SpreeTravel::TravelSample.load_travel_sample("properties_transfer")
 
-      Spree::TravelSample.load_travel_sample("taxonomy_categories")      
-      Spree::TravelSample.load_travel_sample("taxon_category")
+      SpreeTravel::TravelSample.load_travel_sample("taxonomy_categories")      
+      SpreeTravel::TravelSample.load_travel_sample("taxon_category")
 
-      Spree::TravelSample.load_travel_sample("taxons_hotel")
-      Spree::TravelSample.load_travel_sample("taxons_flight")            
-      Spree::TravelSample.load_travel_sample("taxons_program")      
-      Spree::TravelSample.load_travel_sample("taxons_tour")
-      Spree::TravelSample.load_travel_sample("taxons_transfer")
-      Spree::TravelSample.load_travel_sample("taxons_car_rental")
+      SpreeTravel::TravelSample.load_travel_sample("taxons_hotel")
+      SpreeTravel::TravelSample.load_travel_sample("taxons_flight")            
+      SpreeTravel::TravelSample.load_travel_sample("taxons_program")      
+      SpreeTravel::TravelSample.load_travel_sample("taxons_tour")
+      SpreeTravel::TravelSample.load_travel_sample("taxons_transfer")
+      SpreeTravel::TravelSample.load_travel_sample("taxons_car_rental")
             
-      Spree::TravelSample.load_travel_sample("taxonomy_destinations")
-      Spree::TravelSample.load_travel_sample("taxons_destination")
+      SpreeTravel::TravelSample.load_travel_sample("taxonomy_destinations")
+      SpreeTravel::TravelSample.load_travel_sample("taxons_destination")
       
-      Spree::TravelSample.load_travel_sample("taxonomy_flight_destinations")
-      Spree::TravelSample.load_travel_sample("taxons_flight_destination")
+      SpreeTravel::TravelSample.load_travel_sample("taxonomy_flight_destinations")
+      SpreeTravel::TravelSample.load_travel_sample("taxons_flight_destination")
       
-      Spree::TravelSample.load_travel_sample("taxonomy_things_to_do")
-      Spree::TravelSample.load_travel_sample("taxons_thing_to_do")      
+      SpreeTravel::TravelSample.load_travel_sample("taxonomy_things_to_do")
+      SpreeTravel::TravelSample.load_travel_sample("taxons_thing_to_do")      
       
     end
   end
