d3441f1df08b24d762fdf5fd42c49c36dcc22a0c
Miguel Sancho <sanchojaf@gmail.com>
2014-03-25 14:37:01 -0400

    solved issue related with load hotel taxons

--- db/travel_samples/taxons_hotel.rb
+++ db/travel_samples/taxons_hotel.rb
@@ -34,8 +34,8 @@ taxons = [
 
 taxons.each do |taxon_attrs|
   if taxon_attrs[:parent]
-    taxon_attrs[:parent] = Spree::Taxon.find_or_create_by_name!(taxon_attrs[:parent])
-    Spree::Taxon.create!(taxon_attrs)
+    taxon_attrs[:parent] = Spree::Taxon.find_by_name!(taxon_attrs[:parent])
+    Spree::Taxon.find_or_create_by_name(taxon_attrs)
   end
 end
 
