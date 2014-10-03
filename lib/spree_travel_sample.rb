0df5a4907325265012edfa73ef5df72f27de718a
Pedro Quintero Rojas <pqr@uh.cu>
2014-10-03 11:57:44 -0400

    refactoring

--- lib/spree_travel_sample.rb
+++ lib/spree_travel_sample.rb
@@ -5,15 +5,20 @@ module SpreeTravelSample
   class Engine < Rails::Engine
     engine_name 'spree_travel_sample'
 
-    def self.load_travel_samples(what)
+    def self.sample_hash
+      {
+        'common' => [
+          'shipping_categories',
+        ],
+        'hotel' => [
+
+        ]
+      }
 
       #General
-      Spree::TravelSample.load_travel_sample("option_types")
-      Spree::TravelSample.load_travel_sample("option_values")
-      Spree::TravelSample.load_travel_sample("prototypes")
-      Spree::TravelSample.load_travel_sample("product_types")
-      Spree::TravelSample.load_travel_sample("property_types")
-      Spree::TravelSample.load_travel_sample("shipping_categories")
+      #Spree::TravelSample.load_travel_sample("prototypes")
+      #Spree::TravelSample.load_travel_sample("product_types")
+      #Spree::TravelSample.load_travel_sample("property_types")
       #Spree::TravelSample.load_travel_sample("product_option_types")
 
 
@@ -25,14 +30,14 @@ module SpreeTravelSample
       #Spree::TravelSample.load_travel_sample("properties_transfer")
 
       #Taxonomy
-      Spree::TravelSample.load_travel_sample("taxonomies")
+      #Spree::TravelSample.load_travel_sample("taxonomies")
       #Spree::TravelSample.load_travel_sample("taxonomy_categories")
       #Spree::TravelSample.load_travel_sample("taxonomy_destinations")
       #Spree::TravelSample.load_travel_sample("taxonomy_flight_destinations")
       #Spree::TravelSample.load_travel_sample("taxonomy_things_to_do")
 
       #Taxon
-      Spree::TravelSample.load_travel_sample("taxons_hotel")
+      #Spree::TravelSample.load_travel_sample("taxons_hotel")
       #Spree::TravelSample.load_travel_sample("taxon_category")
       #Spree::TravelSample.load_travel_sample("taxons_flight")
       #Spree::TravelSample.load_travel_sample("taxons_program")
@@ -45,20 +50,35 @@ module SpreeTravelSample
 
 
       #Products Hotel
-      Spree::TravelSample.load_travel_sample("hotel_products")
-      Spree::TravelSample.load_travel_sample("hotel_properties")
-      Spree::TravelSample.load_travel_sample("hotel_product_properties")
-      Spree::TravelSample.load_travel_sample("hotel_rates")
-      Spree::TravelSample.load_travel_sample("hotel_variants")
+      #Spree::TravelSample.load_travel_sample("hotel_products")
+      #Spree::TravelSample.load_travel_sample("hotel_properties")
+      #Spree::TravelSample.load_travel_sample("hotel_product_properties")
+      #Spree::TravelSample.load_travel_sample("hotel_rates")
+      #Spree::TravelSample.load_travel_sample("hotel_variants")
       #Spree::TravelSample.load_travel_sample("hotel_assets")
 
       #Products Flights
 
       #Products Programs
 
+    end
 
+    def self.load_travel_dir(dir)
+      files = self.sample_hash[dir]
+      files.each do |file|
+        Spree::TravelSample.load_travel_sample(dir, file)
+      end
+    end
 
-
+    def self.load_travel_samples(what)
+      if what == 'all'
+        self.sample_hash.keys.each do |dir|
+          self.load_travel_dir(dir)
+        end
+      else
+        self.load_travel_dir('common')
+        self.load_travel_dir(what)
+      end
 
     end
   end
