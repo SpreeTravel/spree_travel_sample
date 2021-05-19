d95692e5181f06a999d4a5d29ffa1b01e77f98f8
Raul <raul@backlotcars.com>
2021-05-18 23:07:42 -0300

    Why: This api endpoint was never finished and code is commented, it is better to remove it because the code is useless. this Was mend to be the endpoint to CRUD paxes in the Admin=>Orders=>TravelLineItems

--- db/travel_samples/car/rates.rb
+++ db/travel_samples/car/rates.rb
@@ -20,13 +20,12 @@ cars.each do |car|
         end_date = Date.today + (year - 1) * 365 + season * (365 / seasons) - 1
         rate = Spree::Rate.new
         rate.variant_id = variant.id
-        rate.first_time!
         rate.save
-        rate.set_persisted_option_value(:start_date, start_date)
-        rate.set_persisted_option_value(:end_date, end_date)
-        rate.set_persisted_option_value(:three_six_days, simple = price(100..150, 1, 5..15))
-        rate.set_persisted_option_value(:seven_thirteen_days, double = price(80..120, 2, 10..25))
-        rate.set_persisted_option_value(:fourteen_twentynine_days, triple = price(60..100, 3, 15..40))
+        rate.persist_option_value(Spree::OptionType.find_by(name: 'start_date'), start_date)
+        rate.persist_option_value(Spree::OptionType.find_by(name: 'end_date'), end_date)
+        rate.persist_option_value(Spree::OptionType.find_by(name: 'three_six_days'), simple = price(100..150, 1, 5..15))
+        rate.persist_option_value(Spree::OptionType.find_by(name: 'seven_thirteen_days'), double = price(80..120, 2, 10..25))
+        rate.persist_option_value(Spree::OptionType.find_by(name: 'fourteen_twentynine_days'), triple = price(60..100, 3, 15..40))
         rate.save
         string = "Rate: #{car.name}: (start:#{start_date}) (end:#{end_date})"
         puts '  - created: ' + string
--- db/travel_samples/car/stock.rb
+++ db/travel_samples/car/stock.rb
@@ -3,4 +3,4 @@
 car_variant_ids = Spree::Variant.joins(product: :product_type)
                                 .where('spree_product_types.name = ?', 'car').ids
 
-Spree::StockItem.where(id: car_variant_ids).update_all(count_on_hand: 1)
+Spree::StockItem.where(id: car_variant_ids).update_all(count_on_hand: 10)
